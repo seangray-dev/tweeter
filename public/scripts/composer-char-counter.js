41b79fb51e747e5450a1c0ead977f36429a019e1
Sean Gray <seangray.dev@gmail.com>
2023-03-10 08:03:15 +0000

    add compose tweet with char  counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  const $textarea = $('.new-tweet textarea');
+  const maxChars = 140;
+
+  $textarea.on('input', function () {
+    const $counter = $(this).closest('.new-tweet').find('.counter');
+    const charsLeft = maxChars - $(this).val().length;
+    if (charsLeft <= 0) {
+      $counter.css('color', 'red');
+    } else {
+      $counter.css('color', '');
+    }
+
+    $counter.text(charsLeft);
+    console.log(charsLeft);
+  });
+});
